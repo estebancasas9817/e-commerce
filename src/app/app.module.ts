79de14bb0b7a979f5004ad2540cdd2ae06fb2d38
estebancasas9817 <estebancasas9817@gmail.com>
2021-02-26 13:34:59 -0500

    Implementacion API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,9 +11,6 @@ import { TiendaComponent } from './components/tienda/tienda.component';
 import { AboutComponent } from './components/about/about.component';
 import { CuentaComponent } from './components/cuenta/cuenta.component';
 import { RouterModule } from '@angular/router';
-
-// import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
-// import { ItemProductoComponent } from './components/item-producto/item-producto.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
@@ -21,16 +18,6 @@ import { CartItemComponent } from './components/shopping-cart/cart/cart-item/car
 import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
 import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
-// import { CarroComprasComponent } from './components/carro-compras/carro-compras.component';
-// import { ItemCarroComprasComponent } from './components/item-carro-compras/item-carro-compras.component';
-// import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
-// import { CartComponent } from './components/shopping-cart/cart/cart.component';
-// import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-// import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
-// import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,6 +38,7 @@ import { ProductItemComponent } from './components/shopping-cart/product-list/pr
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     RouterModule.forRoot(
       [
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Cart } from 'src/app/models/cart';
+import { cartUrl } from 'src/config/api';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor(private http: HttpClient) { }
+
+  getCartItems(): Observable<Cart[]> {
+    //TODO: Mapping the obtained result to our CartItem props. (pipe() and map())
+    return this.http.get<Cart[]>(cartUrl).pipe(
+      map((result: any[]) => {
+        let cartItems: Cart[] = [];
+
+        for (let item of result) {
+          let productExists = false
+
+          for (let i in cartItems) {
+            if (cartItems[i].productId === item.product.id) {
+              cartItems[i].qty++
+              productExists = true
+              break;
+            }
+          }
+
+          if (!productExists) {
+            cartItems.push(new Cart(item.id, item.product));
+          }
+        }
+
+        return cartItems;
+      })
+    );
+  }
+
+  addProductToCart(product: Product): Observable<any> {
+    return this.http.post(cartUrl, { product });
+  }
+}
