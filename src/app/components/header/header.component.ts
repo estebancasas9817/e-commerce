654e90b4dcea7f8e9f9cbb3f2c7da8e37c243907
estebancasas9817 <estebancasas9817@gmail.com>
2021-03-02 19:33:12 -0500

    Error componente Cuenta Listo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,10 +11,14 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     const logOut = document.querySelector(".logOut__Link");
+    console.log(logOut);
     const usuario = document.querySelector(".textoUsuario");
     const contenedorLogout = document.querySelector(".contenedor__logOut");
+    const contenedor__verListado = document.querySelector(".contenedor__verListado");
     logOut.addEventListener("click",function(){
+      console.log("entra");
       contenedorLogout.classList.toggle("hidden");
+      contenedor__verListado.classList.toggle("hidden");
       usuario.textContent = "Usuario";
     })
   }
