592f11ee85fea4e78a117a784a04ff349dc9fc60
estebancasas9817 <estebancasas9817@gmail.com>
2021-03-09 17:26:40 -0500

    Proyecto Final primera entrega

--- src/app/components/administrador/cuenta-admin/cuenta-admin.component.ts
+++ src/app/components/administrador/cuenta-admin/cuenta-admin.component.ts
@@ -19,10 +19,16 @@ export class CuentaAdminComponent implements OnInit {
     const textoAdmin = document.querySelector(".textoAdmin");
     this.adminService.getAdmin().subscribe(admin =>{
       this.listaAdmin = admin;
-      
       for(let i in this.listaAdmin){
+        
         if(this.listaAdmin[i].usuario === this.model.adminUsername && this.listaAdmin[i].correo === this.model.adminEmail && this.listaAdmin[i].clave === this.model.adminPassword) {
           textoAdmin.textContent = this.listaAdmin[i].usuario;
+
+          location.href = "panel-admin";
+        }
+        else{
+          location.href = "cuenta-admin";
+          
         }
       }
     }) 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/models/cart';
+import { MessengerService } from 'src/app/services/messenger.service'
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service'
 
 @Component({
   selector: 'app-header',
@@ -6,21 +11,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  cartItems = [];
+  productList: Product[] = []
+  cartTotal = 0
+  @Input() cartItem: any;
 
-  constructor() { }
+  constructor(private msg: MessengerService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.handleSubscription();
+    this.loadCartItems();
+    this.getAllEmployee();
+  }
+
+  eliminarCarro(){
+    for(let i = 1; i < 30; i ++) {
+      this.cartService.eliminarCarros(i).subscribe(
+        (data) => {
+          this.getAllEmployee();
+        });
+
+        this.cartService.eliminarDetalle(i).subscribe(
+          (data) => {
+            this.getAllEmployee();
+          });
+
+      }
+
     const logOut = document.querySelector(".logOut__Link");
-    console.log(logOut);
     const usuario = document.querySelector(".textoUsuario");
     const contenedorLogout = document.querySelector(".contenedor__logOut");
     const contenedor__verListado = document.querySelector(".contenedor__verListado");
-    logOut.addEventListener("click",function(){
-      console.log("entra");
+
       contenedorLogout.classList.toggle("hidden");
       contenedor__verListado.classList.toggle("hidden");
       usuario.textContent = "Usuario";
       localStorage.clear();
+  }
+
+  getAllEmployee() {
+    this.cartService.getAllEmployee();
+  }
+
+  handleSubscription() {
+    this.msg.getMsg().subscribe((product: Product) => {
+      this.loadCartItems();
+    })
+  }
+
+  loadCartItems() {
+    this.cartService.getDetalleItems().subscribe((items: Cart[]) => {
+      this.cartItems = items;
+      
     })
   }
 
